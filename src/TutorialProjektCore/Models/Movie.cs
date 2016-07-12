5606ff5ee53e707c78d05853a0fc95c4305e90e1
Patryk <wolfpoland1@gmail.com>
2016-07-12 17:11:10 +0200

    Kolejny Commit

--- src/TutorialProjektCore/Models/Movie.cs
+++ src/TutorialProjektCore/Models/Movie.cs
@@ -7,5 +7,10 @@ namespace TutorialProjektCore.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string Tytul { get; set; }
+        public DateTime DataWydania { get; set; }
+        public string gatunek { get; set; }
+        public decimal cena { get; set; }
     }
 }
